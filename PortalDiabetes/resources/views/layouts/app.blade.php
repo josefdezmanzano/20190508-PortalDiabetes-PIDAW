ebf02b2e2e0660d9679a98d34e0e5bfe5c5ee69e
Jose <josefernandezmanzano@hotmail.com>
2019-06-07 05:12:16 +0200

    [THIRTEENTH COMMIT]
    Agrego el control de acceso al foro restringido solo a usuarios.

--- PortalDiabetes/resources/views/layouts/app.blade.php
+++ PortalDiabetes/resources/views/layouts/app.blade.php
@@ -30,7 +30,6 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     SweetDeveloping
-                    <!--{{ config('app.name', 'Laravel') }}-->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
